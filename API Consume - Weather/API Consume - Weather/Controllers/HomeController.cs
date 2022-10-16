4dcb7eb7e94d88cfd287d677cb9f5c4022620426
Faizul_Haque <faizul.haque@brainstation-23.com>
2022-10-16 19:39:06 +0600

    weather api - functional

---
+++ API Consume - Weather/API Consume - Weather/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Consume___Weather.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace API_Consume___Weather.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(CityModel model)
+        {
+            string city = model.City;
+            string apiKey = "3a54577fc0895b9f8e4979d4f3adf16f";
+            string server = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+            var client = new RestClient(server);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            IRestResponse response = client.Execute(request);
+            //Console.WriteLine(response.Content);
+
+            WeatherModel weatherModel = JsonConvert.DeserializeObject<WeatherModel>(response.Content);
+
+            ViewBag.weatherModel = weatherModel;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ API Consume - Weather/API Consume - Weather/Models/CityModel.cs
@@ -0,0 +1,8 @@
+﻿namespace API_Consume___Weather.Models
+{
+    public class CityModel
+    {
+        public int Id { get; set; }
+        public string City { get; set; }
+    }
+}
